2562678abfe3d47afa0fd55bddaf4c3198ad4875
Erlyn <Erlynetv@gmail.com>
2020-02-23 18:06:31 -0400

    Se agregó la ruta Form al RouteConfig

--- Tarea4/App_Start/RouteConfig.cs
+++ Tarea4/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace Tarea4
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Form", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
