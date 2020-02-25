dbb61f157b339aab83ffc8dfb93770799f4d093d
Erlyn <Erlynetv@gmail.com>
2020-02-25 10:52:10 -0400

    Se creó el controlador Excel

---
+++ Tarea4/Controllers/ExcelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tarea4.Controllers
+{
+    public class ExcelController : Controller
+    {
+        // GET: Excel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
