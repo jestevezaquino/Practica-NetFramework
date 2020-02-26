1126d9b386f6bae2efe4b910227cd9f7f1d12efe
Jesus <Ing.estevezaquino@gmail.com>
2020-02-26 17:20:00 -0400

    Se creo el controlador Contenido y la vista Noticias, tambien se modifico el archivo _Layout.cshtml.

---
+++ Tarea4/Controllers/ContenidoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tarea4.Controllers
+{
+    public class ContenidoController : Controller
+    {
+        // GET: Contenido
+        public ActionResult Noticias()
+        {
+            return View();
+        }
+    }
+}
